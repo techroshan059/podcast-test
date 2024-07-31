58fb0f49a2d1d85045dbc5ddb116b513bc7e976a
Roshan Shrestha <36528917+techroshan059@users.noreply.github.com>
2024-07-31 11:44:15 +0000

    feed.py updated with title subelement

--- feed.py
+++ feed.py
@@ -10,6 +10,9 @@ with open('feed.yaml','r') as file:
         'xmlns:content':'http://purl.org/rss/1.0/modules/content/'})
 
 channel_element = xml_tree.SubElement(rss_element,'channel')
+
+xml_tree.SubElement(channel_element,'title').text = yaml_data['title']
+
 output_tree = xml_tree.ElementTree(rss_element)
 output_tree.write('podcast.xml',encoding='UTF-8',xml_declaration=True)
 
