31815f5dcad6068f10e180458237796d7189b49b
Roshan Shrestha <36528917+techroshan059@users.noreply.github.com>
2024-07-31 13:03:41 +0100

    Update feed.py

--- feed.py
+++ feed.py
@@ -12,6 +12,8 @@ with open('feed.yaml','r') as file:
 channel_element = xml_tree.SubElement(rss_element,'channel')
 
 xml_tree.SubElement(channel_element,'title').text = yaml_data['title']
+xml_tree.SubElement(channel_element,'subtitle').text = yaml_data['subtitle']
+
 
 output_tree = xml_tree.ElementTree(rss_element)
 output_tree.write('podcast.xml',encoding='UTF-8',xml_declaration=True)
